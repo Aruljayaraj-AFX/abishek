d898cb195d371ff82a077bd5a6aa839f6369c0ed
Arul Jayaraj <aruljayarajj826@gmail.com>
2025-05-03 22:13:07 +0530

    Update requirements files

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from datetime import datetime
 
-app=FastAPI()
+app = FastAPI()
 
 @app.get("/")
 async def health_check():
-    return "Happy"
+    # Get the current date and time
+    current_datetime = datetime.now()
+    
+    # Return the current date and time as a string
+    return {"message": "Happy", "current_datetime": current_datetime.strftime("%Y-%m-%d %H:%M:%S")}
